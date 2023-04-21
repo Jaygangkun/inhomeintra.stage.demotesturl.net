3170ade53d32d2ca9ceced2380ee1eabfc5bc3fc
JayGangkun <jaygangkun@hotmail.com>
2023-04-21 17:24:17 -0400

    no message

---
+++ wp-content/plugins/document-library-pro/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'barn2/document-library-pro',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '4975b2427c0654e82f3eeeab32b3fa9d22151ed0',
+        'type' => 'wordpress-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'barn2/document-library-pro' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '4975b2427c0654e82f3eeeab32b3fa9d22151ed0',
+            'type' => 'wordpress-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
